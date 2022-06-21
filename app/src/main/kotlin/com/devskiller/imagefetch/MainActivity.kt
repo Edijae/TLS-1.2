47f2659191f9ef48ebfde656de756ff3c67e3ad1
Samuel <samuel.karanja@andela.com>
2022-06-21 14:47:38 +0300

    Solution

--- app/src/main/kotlin/com/devskiller/imagefetch/MainActivity.kt
+++ app/src/main/kotlin/com/devskiller/imagefetch/MainActivity.kt
@@ -10,9 +10,13 @@ import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.OkHttp3Downloader
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
+import okhttp3.TlsVersion
 import java.lang.ref.WeakReference
+import java.security.KeyStore
 import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 import javax.net.ssl.X509TrustManager
 
 class MainActivity : AppCompatActivity() {
@@ -82,10 +86,45 @@ class MainActivity : AppCompatActivity() {
     private fun OkHttpClient.Builder.enableTls12OnPreLollipop(): OkHttpClient.Builder = apply {
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
             // START CHANGES
+            try{
+                //Returns a SSLContext object that implements the specified secure socket protocol.
+                SSLContext.getInstance("TLSv1.2").also{
+                    it.init(null,null,null)
+                    findX509TrustManager()?.also{ trust ->
+                        sslSocketFactory(Tls12SocketFactory(it.socketFactory), trust)
+                        connectionSpecs(arrayListOf(
+                            ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                                .tlsVersions(TlsVersion.TLS_1_2)
+                                .build(),
+                            ConnectionSpec.COMPATIBLE_TLS,
+                            ConnectionSpec.CLEARTEXT)
+                        )
+                    }
+                }
+            }catch(exc: Exception){
+                Log.e("enableTls12","exception", exc)
+            }
             // END CHANGES
         }
+        return this
     }
 
+    //Returns instance of X509TrustManager which manage which X509 certificates
+    // may be used to authenticate the remote side of a secure socket
+    private fun findX509TrustManager():X509TrustManager?{
+        TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()).apply{
+            init(null as KeyStore?)
+            trustManagers.also{ it ->
+                if(it.isEmpty()){
+                    Log.e("TLS", "No TrustManager instances")
+                }else{
+                    (it[0] as? X509TrustManager)?.also{ return it}
+                    Log.e("TLS", "TrustManager[0] is not X509")
+                }
+            }
+            return null
+        }
+    }
     private fun imageFetchedSuccessfully() = Log.v(LOG_TAG, "image fetched successfully!")
 
     private fun imageFetchedWithFailure(exception: Exception) = Log.v(LOG_TAG, "image fetched unsuccessfully!:$exception")
---
+++ app/src/main/kotlin/com/devskiller/imagefetch/Tls12SocketFactory.kt
@@ -0,0 +1,53 @@
+package com.devskiller.imagefetch
+
+import java.io.IOException
+import java.net.InetAddress
+import java.net.Socket
+import java.net.UnknownHostException
+import javax.net.ssl.SSLSocket
+import javax.net.ssl.SSLSocketFactory
+
+class Tls12SocketFactory(internal val delegate: SSLSocketFactory): SSLSocketFactory(){
+
+    private  val TLS_V12_ONLY = arrayOf("TLSv1.2")
+
+    override fun getDefaultCipherSuites(): Array<String>{
+        return delegate.defaultCipherSuites
+    }
+
+    override fun getSupportedCipherSuites(): Array<String>{
+        return delegate.supportedCipherSuites
+    }
+
+    @Throws(IOException::class)
+    override fun createSocket(s: Socket, host:String, port:Int, autoclose:Boolean):Socket?{
+        return patch(delegate.createSocket(s,host, port, autoclose))
+    }
+
+    @Throws(IOException::class)
+    override fun createSocket(host:String, port:Int):Socket?{
+        return patch(delegate.createSocket(host, port))
+    }
+
+    @Throws(IOException::class, UnknownHostException::class)
+    override fun createSocket(host:String, port:Int, localHost: InetAddress, localPort:Int):Socket?{
+        return patch(delegate.createSocket(host, port, localHost, localPort))
+    }
+
+    @Throws(IOException::class)
+    override fun createSocket(host:InetAddress, port:Int):Socket?{
+        return patch(delegate.createSocket(host, port))
+    }
+
+    @Throws(IOException::class)
+    override fun createSocket(address:InetAddress, port:Int, localAddress:InetAddress,localPort:Int):Socket?{
+        return patch(delegate.createSocket(address, port, localAddress, localPort))
+    }
+
+    private fun patch(socket:Socket):Socket{
+        if(socket is SSLSocket){
+            socket.enabledProtocols = TLS_V12_ONLY
+        }
+        return socket;
+    }
+}
